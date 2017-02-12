1729e4660409ebfa038e90ad7130e27110483430
Mateo Murphy <mateo.murphy@gmail.com>
2017-02-12 11:43:04 -0500

    Alphabetize functions in source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import _ from 'lodash'
 import * as utils from './utils'
 
+export function first(coll, path) {
+  return _.first(get(coll, path))
+}
+
 export function get(coll, path) {
   if (path.length === 0) {
     return coll
@@ -16,12 +20,36 @@ export function get(coll, path) {
   }
 }
 
-export function first(coll, path) {
-  return _.first(get(coll, path))
-}
-
 export function last(coll, path) {
   return _.last(get(coll, path))
+}
+
+export function merge(coll, path, value) {
+  return update(coll, path, (target) => Object.assign({}, target, value))
+}
+
+export function pop(coll, path) {
+  return update(coll, path, (target) => target.slice(0, -1))
+}
+
+export function push(coll, path, value) {
+  return update(coll, path, (target) => [...target, value])
+}
+
+export function set(coll, path, value) {
+  return update(coll, path, (target) => value)
+}
+
+export function shift(coll, path) {
+  return update(coll, path, (target) => target.slice(1))
+}
+
+export function slice(coll, path) {
+  return update(coll, path, (target) => target.slice(arguments[2], arguments[3]))
+}
+
+export function unshift(coll, path, value) {
+  return update(coll, path, (target) => [value, ...target])
 }
 
 export function update(coll, path, fn) {
@@ -38,32 +66,3 @@ export function update(coll, path, fn) {
     return utils.set(coll, key, update(coll[key], path, fn))
   }
 }
-
-export function merge(coll, path, value) {
-  return update(coll, path, (target) => Object.assign({}, target, value))
-}
-
-export function pop(coll, path) {
-  return update(coll, path, (target) => target.slice(0, -1))
-}
-
-export function set(coll, path, value) {
-  return update(coll, path, (target) => value)
-}
-
-export function shift(coll, path) {
-  return update(coll, path, (target) => target.slice(1))
-}
-
-export function slice(coll, path, start = undefined, end = undefined) {
-  return update(coll, path, (target) => target.slice(start, end))
-}
-
-export function push(coll, path, value) {
-  return update(coll, path, (target) => [...target, value])
-}
-
-export function unshift(coll, path, value) {
-  return update(coll, path, (target) => [value, ...target])
-}
-
